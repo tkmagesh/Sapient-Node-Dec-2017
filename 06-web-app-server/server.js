f3112b0f63dc5ceb0a8df93a20585383ea96e755
Magesh <tkmagesh77@gmail.com>
2017-12-02 15:20:39 +0530

    web app server for calculator - solution

--- 06-web-app-server/server.js
+++ 06-web-app-server/server.js
@@ -11,19 +11,25 @@ let server = http.createServer(function(req, res){
 		rawData = querystring.parse(urlObj.query),
 		resourcePath = path.join(__dirname, urlObj.pathname);
 
-	if (urlObj.pathname !== '/calculator'){
+	if (urlObj.pathname === '/calculator-get.html'){
+		if (!fs.existsSync(resourcePath)){
+			res.statusCode = 404;
+			res.end();
+			return;
+		}
+		fs.createReadStream(resourcePath).pipe(res);
+	} else if (urlObj.pathname === '/calculator'){
+		let op = rawData.op,
+			n1 = parseInt(rawData.n1),
+			n2 = parseInt(rawData.n2);
+		
+		let result = calculator[op](n1, n2);
+		res.write(result.toString());
+		res.end();
+	} else {
 		res.statusCode = 404;
 		res.end();
-		return;
 	}
-	
-	let op = rawData.op,
-		n1 = parseInt(rawData.n1),
-		n2 = parseInt(rawData.n2);
-	
-	let result = calculator[op](n1, n2);
-	res.write(result.toString());
-	res.end();
 });
 
 server.listen(8085);

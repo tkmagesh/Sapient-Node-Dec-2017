5b8c54635e96d9d69fe0b117f67164a60d16c48d
Magesh <tkmagesh77@gmail.com>
2017-12-02 16:29:11 +0530

    handling post in web app server

--- 06-web-app-server/server.js
+++ 06-web-app-server/server.js
@@ -14,7 +14,7 @@ function isStatic(resource){
 let server = http.createServer(function(req, res){
 
 	let urlObj = url.parse(req.url),
-		rawData = querystring.parse(urlObj.query),
+		queryData = querystring.parse(urlObj.query),
 		resourcePath = path.join(__dirname, urlObj.pathname);
 
 	if (isStatic(urlObj.pathname)){
@@ -24,18 +24,39 @@ let server = http.createServer(function(req, res){
 			return;
 		}
 		fs.createReadStream(resourcePath).pipe(res);
-	} else if (urlObj.pathname === '/calculator'){
-		let op = rawData.op,
-			n1 = parseInt(rawData.n1),
-			n2 = parseInt(rawData.n2);
+	} else if (urlObj.pathname === '/calculator' && req.method === 'GET '){
+		let op = queryData.op,
+			n1 = parseInt(queryData.n1),
+			n2 = parseInt(queryData.n2);
 		
 		let result = calculator[op](n1, n2);
 		res.write(result.toString());
 		res.end();
+	} else if (urlObj.pathname === '/calculator' && req.method === 'POST'){
+
+		let data = '';
+		req.on('data', function(chunk){
+			data += chunk;
+		});
+		req.on('end', function(){
+			let bodyData = querystring.parse(data);
+			let op = bodyData.op,
+				n1 = parseInt(bodyData.n1),
+				n2 = parseInt(bodyData.n2);
+			
+			let result = calculator[op](n1, n2);
+			res.write(result.toString());
+			res.end();
+		});
+
 	} else {
 		res.statusCode = 404;
 		res.end();
 	}
+});
+
+process.on('uncaughtException', function(err){
+	console.log('something went wrong', err);
 });
 
 server.listen(8085);

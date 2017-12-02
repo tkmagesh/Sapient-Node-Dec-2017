14b5a6bcc87f2df3eebb52357e687f7b30ac14c3
Magesh <tkmagesh77@gmail.com>
2017-12-02 11:36:38 +0530

    modularity

---
+++ 02-modularity/calculatorClient.js
@@ -0,0 +1,8 @@
+let calculator = require('./calculator');
+console.log('[@calculatorClient.js] calculator -> ', calculator);
+let x = 100,
+	y = 50;
+console.log(calculator.add(x,y));
+console.log(calculator.subtract(x,y));
+console.log(calculator.multiply(x,y));
+console.log(calculator.divide(x,y));

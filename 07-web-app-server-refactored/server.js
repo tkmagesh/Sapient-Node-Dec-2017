68c7c4d5291a63669dbf36f91f41be86aceb23f5
Magesh <tkmagesh77@gmail.com>
2017-12-03 10:59:15 +0530

    web app server - refactored - part - 2

--- 07-web-app-server-refactored/server.js
+++ 07-web-app-server-refactored/server.js
@@ -3,21 +3,12 @@ const dataParser = require('./dataParser');
 const serveStatic = require('./serveStatic');
 const calculatorHandler = require('./calculatorHandler');
 const notFoundHandler = require('./notFoundHandler');
+const app = require('./app');
 
-let _middlewares = [dataParser, serveStatic, calculatorHandler, notFoundHandler];
+app.use(dataParser);
+app.use(serveStatic);
+app.use(calculatorHandler);
+app.use(notFoundHandler);
 
-let server = http.createServer(function(req, res){
-	function exec(req, res, middlewares){
-		let first = middlewares[0],
-			remaining = middlewares.slice(1),
-			next = function(){
-				exec(req, res, remaining)
-			};
-		if (typeof first === 'function')
-			first(req, res, next);
-	}
-	exec(req, res, _middlewares);
-});
-
-server.listen(8085);
+http.createServer(app).listen(8085);
 

7c0307475fb5663eea1e19b26f7bc858d4397e1f
Magesh <tkmagesh77@gmail.com>
2017-12-03 15:28:58 +0530

    listing & adding new tasks

--- 09-task-manager/routes/index.js
+++ 09-task-manager/routes/index.js
@@ -3,15 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	let products = [
-		'Pen',
-		'Pencil'
-	];
-	let viewData = {
-		title : 'Product List',
-		products : products
-	};
-  res.render('index', viewData);
+	var options = {
+    	root: __dirname + '/public/'
+  	};
+
+  var fileName = 'index.html';
+  res.sendFile(fileName);
 });
 
 module.exports = router;
--- 09-task-manager/routes/taskRoutes.js
+++ 09-task-manager/routes/taskRoutes.js
@@ -10,4 +10,11 @@ router.get('/', function(req, res, next) {
 	res.json(tasks);
 });
 
+router.post('/', function(req, res,next){
+	let newTaskId = tasks.reduce((prevResult, task) => prevResult > task.id ? prevResult : task.id, 0) + 1;
+	let newTaskData = req.body;
+	newTaskData.id = newTaskId;
+	tasks.push(newTaskData);
+	res.status(201).json(newTaskData);
+})
 module.exports = router;
